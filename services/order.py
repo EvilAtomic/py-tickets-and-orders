349b555dab3e0df589ceeaaae280412af0e1ec6c
artem <artemasarkisan598@gmail.com>
2026-04-26 22:51:45 +0300

    py-Tickets and orders

--- services/order.py
+++ services/order.py
@@ -1,21 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import transaction
-import datetime
 from django.db.models import QuerySet
-from db.models import Ticket
-from db.models import Order
+from db.models import Order, Ticket
 
 
-def get_orders(username: str = None) -> QuerySet[Order]:
+def get_orders(username: str = None) -> QuerySet:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
 
-def create_order(tickets: list[dict], username: str| None, data: datetime) -> None:
+def create_order(tickets: list, username: str, date=None) -> None:
     with transaction.atomic():
         user = User.objects.get(username=username)
-        order = Order.objects.create(user=user, created_at=data)
-
+        order = Order.objects.create(user=user, created_at=date)
         for ticket in tickets:
             Ticket.objects.create(
                 order=order,
@@ -23,5 +20,3 @@ def create_order(tickets: list[dict], username: str| None, data: datetime) -> No
                 seat=ticket["seat"],
                 movie_session_id=ticket["movie_session"]
             )
-
-
