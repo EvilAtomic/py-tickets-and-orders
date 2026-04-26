440767d977ccd8e619526e36ade38d154e6365c7
artem <artemasarkisan598@gmail.com>
2026-04-26 23:46:51 +0300

    py-Tickets and orders

--- db/models.py
+++ db/models.py
@@ -98,14 +98,18 @@ class Ticket(models.Model):
 
         if not (1 <= self.row <= hall.rows):
             raise ValidationError({
-                "row": [f"row number must be in available range:"
-                        f" (1, {hall.rows})"]
+                "row": [
+                    f"row number must be in available range: "
+                    f"(1, rows): (1, {hall.rows})"
+                ]
             })
 
         if not (1 <= self.seat <= hall.seats_in_row):
             raise ValidationError({
-                "seat": [f"seat number must be in available range:"
-                         f" (1, {hall.seats_in_row})"]
+                "seat": [
+                    f"seat number must be in available range: "
+                    f"(1, seats_in_row): (1, {hall.seats_in_row})"
+                ]
             })
 
     def save(self, *args, **kwargs) -> None:
